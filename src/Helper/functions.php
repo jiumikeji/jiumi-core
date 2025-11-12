8d36c76c659f942e483d19deb495876d8d38c9b6
jiumikeji <31468309+jiumikeji@users.noreply.github.com>
2025-11-12 12:03:25 +0800

    Add files via upload
    精准授权1

--- src/Helper/functions.php
+++ src/Helper/functions.php
@@ -258,9 +258,11 @@ if (!function_exists('get_user_perm_info')) {
      * 获取用户的数据权限-用户ID集合
      * @param int $userId 用户ID
      * @param string|array $permIds 权限编码 xx:xx
+     * @param string $where 查询条件 OR / AND
+     * @param bool|null $scope 是否权限控制
      * @return array
      */
-    function get_user_perm_info(int $userId, string|array $permIds,string $where = 'OR'): array
+    function get_user_perm_info(int $userId, string|array $permIds, string $where = 'OR', bool $scope = null): array
     {
         $result = [
             'admin' => false,
@@ -268,6 +270,14 @@ if (!function_exists('get_user_perm_info')) {
             'isHasSelf' => false,
             'query' => null,
         ];
+        if (is_null($scope)) {
+            $scope = Context::get("sys_perm_scope", true);
+        }
+        if (!config('jiumiadmin.data_scope_enabled') || !$scope) {
+            $result['admin'] = true;
+            return $result;
+        }
+
         if (env('SUPER_ADMIN') == $userId) {
             $result['admin'] = true;
             return $result;
@@ -295,12 +305,12 @@ if (!function_exists('get_user_perm_info')) {
             ->where('ru.user_id', $userId)
             ->whereNull('r.deleted_at')
             ->where('r.status', 1);
-        if (!empty($permIds)){
+        if (!empty($permIds)) {
             if (count($permIds) > 1) {
-                if (strtoupper($where) == 'OR'){
+                if (strtoupper($where) == 'OR') {
                     $query = $query->whereIn('m.code', $permIds);
-                }else{
-                    foreach ($permIds as $permId){
+                } else {
+                    foreach ($permIds as $permId) {
                         $query = $query->where('m.code', $permId);
                     }
                 }
@@ -313,11 +323,11 @@ if (!function_exists('get_user_perm_info')) {
         if (empty($roles)) {
             return $result;
         }
-        $isHasSelf=false;
+        $isHasSelf = false;
         $deptIds = [];
         $customDeptIds = [];
         $childrenDeptIds = [];
-        $roleCache= [];
+        $roleCache = [];
         foreach ($roles as $role) {
             if (isset($roleCache[$role['id']])) continue;
             $roleCache[$role['id']] = true;
@@ -328,7 +338,7 @@ if (!function_exists('get_user_perm_info')) {
             }
             if ($role['data_scope'] == SystemRole::CUSTOM_SCOPE) {
                 //自定义
-                if (!isset($customDeptIds[$role['id']])){
+                if (!isset($customDeptIds[$role['id']])) {
                     $customDeptIds[$role['id']] = true;
                     $ids = Db::table('system_role_dept')->where('role_id', $role['id'])
                         ->pluck('dept_id')->toArray();
@@ -355,8 +365,8 @@ if (!function_exists('get_user_perm_info')) {
                     $allIds[$id] = $id;
                 }
                 $allIds = array_values($allIds);
-                $key=implode(',',$allIds);
-                if (!isset($childrenDeptIds[$key])){
+                $key = implode(',', $allIds);
+                if (!isset($childrenDeptIds[$key])) {
                     $childrenDeptIds[$key] = true;
                     //获取当前和子部门所有
                     $ids = Db::table('system_dept')->where('status', 1)->whereNull('deleted_at')
@@ -373,12 +383,12 @@ if (!function_exists('get_user_perm_info')) {
                 continue;
             }
             if ($role['data_scope'] == SystemRole::SELF_SCOPE) {
-                $isHasSelf=true;
+                $isHasSelf = true;
             }
         }
         $result['isHasSelf'] = $isHasSelf;
         $deptIds = array_values($deptIds);
-        $query=Db::table('system_user as u')
+        $query = Db::table('system_user as u')
             ->leftJoin('system_user_dept as ud', 'u.id', '=', 'ud.user_id')
             ->select(['u.id']);
         if ($isHasSelf) {
@@ -386,7 +396,7 @@ if (!function_exists('get_user_perm_info')) {
             if (!empty($deptIds)) {
                 $query = $query->orWhereIn('ud.dept_id', $deptIds)->orWhereIn('u.dept_id', $deptIds);
             }
-        }else if (!empty($deptIds)){
+        } else if (!empty($deptIds)) {
             $query = $query->whereIn('ud.dept_id', $deptIds)->orWhereIn('u.dept_id', $deptIds);
         }
         $result['deptIds'] = $deptIds;
@@ -400,13 +410,14 @@ if (!function_exists('get_context_perm_info')) {
      * @param int $userId 用户ID
      * @return array
      */
-    function get_context_perm_info($userId): array{
-        $permWhere = Context::get('sys_perm_where','OR');
-        $permCodes = Context::get('sys_perm_codes',[]);
-        $permInfo=Context::get("sys_perm_cache_".$userId);
-        if (!$permInfo){
-            $permInfo=get_user_perm_info($userId,$permCodes,$permWhere);
-            Context::set("sys_perm_cache",$permInfo);
+    function get_context_perm_info($userId): array
+    {
+        $permWhere = Context::get('sys_perm_where', 'OR');
+        $permCodes = Context::get('sys_perm_codes', []);
+        $permInfo = Context::get("sys_perm_cache_" . $userId);
+        if (!$permInfo) {
+            $permInfo = get_user_perm_info($userId, $permCodes, $permWhere);
+            Context::set("sys_perm_cache", $permInfo);
         }
         return $permInfo;
     }
